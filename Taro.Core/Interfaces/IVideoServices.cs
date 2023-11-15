02b7dc00604f9d2b1f07bfddfbd227f219a90005
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-15 22:25:05 +0200

    Add IVideo Interface

---
+++ Taro.Core/Interfaces/IVideoServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taro.Core.Dtos.Responses;
+using Taro.Core.Dtos.VideoDtos;
+
+namespace Taro.Core.Interfaces
+{
+    public interface IVideoServices
+    {
+        Task<Response<long>> AddVideo(AddVideoDto addVideoDto);
+        Task<Response<bool>> DeleteVideo(int Id);
+        Task<Response<VideoDetailsDto>> GetVideo(int Id);
+        Task<Response<bool>> UpdateVideo(AddVideoDto addVideoDto, int Id);
+        Task<Response<List<VideoDetailsDto>>> GetCourseVideos(long courseId);
+    }
+}
