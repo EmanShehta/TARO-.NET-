9a19210e90f5cbe0306a83527b61e51d9c634751
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-15 21:55:57 +0200

    Add Video model

---
+++ Taro.Core/Entities/Models/CourseModels/Video.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taro.Core.Entities.Models.CourseModels
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public long CourseId { get; set; }
+    }
+}
