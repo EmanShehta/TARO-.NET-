ce86d26dcbb54cd61e8c22f0f45f3c32673ef285
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-15 21:55:28 +0200

    Add Cousre model

---
+++ Taro.Core/Entities/Models/CourseModels/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taro.Core.Entities.Models.CourseModels
+{
+    public class Course
+    {
+        public long Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public double Rate { get; set; }
+        public string ImagePath { get; set; } = string.Empty;
+        public List<Video> Videos { get; set; } 
+    }
+}
