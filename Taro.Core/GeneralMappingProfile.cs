09a120e430e6bdc5269bfc877dafde81e8efaa2f
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-15 22:25:52 +0200

    Create class for mapping

---
+++ Taro.Core/GeneralMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System.Diagnostics.Metrics;
+using Taro.Core.Dtos.CourseDtos;
+using Taro.Core.Dtos.VideoDtos;
+using Taro.Core.Entities.Models.CourseModels;
+
+namespace Core.MappingProfiles
+{
+    public class GeneralMappingProfile : Profile
+    {
+        public GeneralMappingProfile()
+        {
+            #region
+            CreateMap<Course,AddCourseDto>().ReverseMap();
+            CreateMap<Course, CourseDetailsDto>().ReverseMap();
+        
+            #endregion
+
+            #region
+            CreateMap<Video, AddVideoDto>().ReverseMap();
+            CreateMap<Video, VideoDetailsDto>().ReverseMap();
+            #endregion
+        }
+    }
+}
