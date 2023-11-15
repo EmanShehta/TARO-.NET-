c48725f23131c4f6105a013c231471ba5cf90f0b
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-15 21:54:18 +0200

    Add Video Dto

---
+++ Taro.Core/Dtos/VideoDtos/AddVideoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taro.Core.Dtos.VideoDtos
+{
+    public class AddVideoDto
+    {
+        public string Url { get; set; } = string.Empty;
+        public long CourseId { get; set; }
+    }
+}
