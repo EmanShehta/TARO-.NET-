a7d7f2be1c1f628a86e7c4a91547b53a5c93451e
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-15 21:53:17 +0200

    Add Course Details Dto

---
+++ Taro.Core/Dtos/CourseDtos/CourseDetailsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taro.Core.Entities.Models.CourseModels;
+
+namespace Taro.Core.Dtos.CourseDtos
+{
+    public class CourseDetailsDto
+    {
+        public long Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public double Rate { get; set; }
+        public string ImagePath { get; set; } = string.Empty;
+        public List<Video> Videos { get; set; }
+    }
+}
