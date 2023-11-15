758a1229a194138865b433e6ebe307bdce3eabd7
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-15 22:30:25 +0200

    Rename AppIdentitityDbContext to AppDbContext and move it from this folder

---
+++ Taro.Repository/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Taro.Core.Entities.Models.CourseModels;
+
+namespace Taro.Repository.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Video> videos { get; set; }
+    }
+}
