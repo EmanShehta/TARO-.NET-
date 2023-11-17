fe32e10748345b3276e8b914be7e7f2eb095cf88
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-17 14:40:34 +0200

    Add Role model

---
+++ Taro.Core/Entities/Roles/Role.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Taro.Core.Entities.Roles
+{
+    public class Role :  IdentityRole
+    {
+        public DateTime? CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
