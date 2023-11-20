ebbccb4406c012fee4900017abdd1b41aa250f58
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-20 16:03:44 +0200

    Add Quiz Table to database

--- Taro.Repository/Migrations/20231120134851_AddQuizTable.Designer.cs
+++ Taro.Repository/Migrations/20231120134851_AddQuizTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Taro.Repository.Context;
 
@@ -11,9 +12,10 @@ using Taro.Repository.Context;
 namespace Taro.Repository.Identity.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231120134851_AddQuizTable")]
+    partial class AddQuizTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Taro.Repository/Migrations/20231120134851_AddQuizTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Taro.Repository.Identity.Migrations
+{
+    public partial class AddQuizTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quizzes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    A = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Q = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quizzes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quizzes");
+        }
+    }
+}
