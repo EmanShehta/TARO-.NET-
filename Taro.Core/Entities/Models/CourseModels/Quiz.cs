9d64e95c3def43cf6b4c8e96df00a97b7290dc56
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-20 15:56:04 +0200

    Add Quiz Model

---
+++ Taro.Core/Entities/Models/CourseModels/Quiz.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taro.Core.Entities.Models.CourseModels
+{
+    public class Quiz
+        {
+            [Required]
+            public int Id { get; set; }
+            public String A { get; set; }
+            public String Q { get; set; }
+    }
+    
+}
